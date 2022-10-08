23c3b9d7b1ae4ad8717098f2a122ebcc1548a0cf
by-sabbir <me@sabbir.dev>
2022-10-09 02:13:16 +0600

    chore: integrating services

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,30 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
-	consulApi "github.com/by-sabbir/consul-kv-discovery/pkg/consul"
+	_ "github.com/by-sabbir/consul-kv-discovery/internal/consul"
 )
-
-var (
-	CONSUL_ADDR  = "consul:8500"
-	SERVICE_ID   = "go-service"
-	SERVICE_NAME = "go-service-test"
-	SERVICE_PORT = 8000
-	SERVICE_HOST = "0.0.0.0"
-)
-
-func init() {
-	cli, err := consulApi.NewClient(CONSUL_ADDR)
-	if err != nil {
-		log.Fatalf("can't initiate consul client: %+v\n", err)
-	}
-	if err := cli.Register(SERVICE_ID); err != nil {
-		log.Println("error registering... ", err)
-	}
-}
 
 func main() {
 	if err := Run(); err != nil {
@@ -35,8 +16,7 @@ func main() {
 
 func Run() error {
 	http.HandleFunc("/health", healthcheck)
-	srvString := fmt.Sprintf("%s:%d", SERVICE_HOST, SERVICE_PORT)
-	if err := http.ListenAndServe(srvString, nil); err != nil {
+	if err := http.ListenAndServe(":8000", nil); err != nil {
 		return err
 	}
 	return nil
---
+++ internal/consul/registry.go
@@ -0,0 +1,47 @@
+package consul
+
+import (
+	"fmt"
+	"log"
+
+	consulApi "github.com/by-sabbir/consul-kv-discovery/pkg/consul"
+)
+
+type ServiceDefinition struct {
+	ConsulAddr  string
+	ServiceId   string
+	ServiceHost string
+	ServicePort int
+}
+
+func init() {
+	var sd ServiceDefinition
+	sd.ConsulAddr = "consul:8500"
+	sd.ServiceId = "go-service"
+	sd.ServiceHost = "0.0.0.0"
+	sd.ServicePort = 8000
+
+	cli, err := consulApi.NewClient(sd.ConsulAddr)
+	if err != nil {
+		log.Fatalf("can't initiate consul client: %+v\n", err)
+	}
+	if err := cli.Register(sd.ServiceId); err != nil {
+		log.Println("error registering... ", err)
+	}
+	log.Println("service registered: ", sd.ServiceId)
+
+	consulStore := consulApi.NewKVClient(*cli)
+
+	srvString := fmt.Sprintf("%s:%d", sd.ServiceHost, sd.ServicePort)
+	if err := consulStore.PutKV(sd.ServiceId, srvString); err != nil {
+		log.Println("error creating key: ", err)
+
+	}
+
+	url, err := consulStore.GetKV("apigw")
+	if err != nil {
+		log.Println("could not get kv for apigw: ", err)
+	}
+	log.Println("apigw baseUrl: ", url)
+
+}
